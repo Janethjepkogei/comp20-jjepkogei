851c8dee40fd44c014a72eb4d07ce5c3c54fc222
Janeth Jepkogei <janeth.jepkogei@tufts.edu>
2015-10-17 00:43:35 -0400

    modifies game.js to have background image and two birds

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,1 +1,18 @@
-// Replace this with your code...
+function init() {
+	console.log("here");
+      var canvas = document.getElementById("game_canvas");
+      if (canvas.getContext) {
+        var context = canvas.getContext('2d');
+        img = new Image();
+        img1 = new Image();
+        img2 = new Image();
+        img.onload = function(){
+        	context.drawImage(img,0,0);
+        	context.drawImage(img1, 75, 110, 50, 38, 50, 20, 115, 50);
+        	context.drawImage(img2, 120, 100, 50, 50, 130, 10, 100, 50);
+        };
+        img.src = './duckhunt-background.gif';
+        img1.src = './duckhunt_various_sheet.png';
+        img2.src = './duckhunt_various_sheet.png';
+    }
+}
