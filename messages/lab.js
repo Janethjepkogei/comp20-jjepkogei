c9cddb6bf69ba446f6f696f76c847c6a74b27961
Janeth Jepkogei <janeth.jepkogei@tufts.edu>
2015-10-22 20:17:20 -0400

    lab 6 first commit

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,20 @@
-// Your JavaScript goes here...
+
+function parse(){
+	var request = new XMLHttpRequest();
+	var message = "noMessage";
+	request.open('GET', 'http://messagehub.herokuapp.com/messages.json',true);
+
+	request.onreadystatechange = function(){
+	 		 	if (request.readyState == 4 && request.status == 200){
+	 				data = request.responseText;
+	 				message = JSON.parse(data);
+	 				elem = document.getElementById("messages");
+
+	 				for(var i =0; i < message.length; i++ ){
+	 					elem.innerHTML +="<p>" + message[i].content + " "+message[i].username + "</p>"
+	 				}
+	 	}
+
+	 };
+	 request.send(null);
+}
