3084d4facbe8107fd74d6740998707361b1e08c8
Janeth Jepkogei <janeth.jepkogei@tufts.edu>
2015-11-01 16:42:36 -0500

    Loads and displays locations of everyone in the class on the map

--- mapchat/googleMaps.js
+++ mapchat/googleMaps.js
@@ -1,11 +1,13 @@
 var myLat =0;
 var myLng = 0;
-var request =  new XMLHttpRequest;
 var message = "noMessage";
 var landmark = new google.maps.LatLng(myLat,myLng);
 var map;
 var marker;
+var login;
 var infowindow = new google.maps.InfoWindow();
+var mycurrentLat;
+var mycurrentLng;
 var myOptions = {
 		zoom: 13,
 		center: landmark,
@@ -16,50 +18,94 @@ function init()
 {
 	map = new google.maps.Map(document.getElementById("myMap"), myOptions);
 
-	getLocation();
+	
+	getMyLocation();
 }
 
-function getLocation(){
-	if(navigator.geolocation){
-		navigator.geolocation.getCurrentPosition(function(position) {
-			myLat = position.coords.latitude;
-			myLng = position.coords.longitude;
+function getLocation()
+{
+	
+	if(navigator.geolocation)
+	{
+		console.log(message[0]);
+		for(var i =0; i < message.length; i++)
+		 {
+			myLat = message[i].lat;
+			myLng = message[i].lng;
+			login = message[i].login;
+			note = message[i].message;
+			distanceDiff = distanceFromMe(myLat,myLng);
 			renderMap();
-		});
+		}
 	}
-//parse();
+
 }
+ function getMyLocation() {
+				if (navigator.geolocation) { 
+					navigator.geolocation.getCurrentPosition(function(position) {
+						 mycurrentLat = position.coords.latitude;
+						 mycurrentLng = position.coords.longitude;
+						 parse();
+					});
+			}	
+	}
 
 function renderMap(){
 	landmark = new google.maps.LatLng(myLat, myLng);
 
 	map.panTo(landmark);
-
 		marker = new google.maps.Marker({
 			position: landmark,
-			title: "Here I am"
+			title: login,
+			message: note,
+			
 		});
 
 	marker.setMap(map);
 
-	google.maps.event.addListener(marker, 'click', function() {
-		infowindow.setContent(marker.title);
+		google.maps.event.addListener(marker, 'click', function() {
+		if(myLat == mycurrentLat && myLng == mycurrentLng){
+			infowindow.setContent(marker.title + "<br/> message: " + marker.message);
+
+		}
+		else{
+			infowindow.setContent(marker.title + "<br/> message: " + marker.message + "<br/> Distance from me: " );
+		}
 		infowindow.open(map, marker);
 	});
 
 }
 
-function parse(){
-	request.open('POST', 'https://secret-about-box.herokuapp.com/sendLocation', true);
+function distanceFromMe(var myLat, var myLng)
+{
+	
 
+}
+
+
+function parse(){
+
+var request =  new XMLHttpRequest;
+
+	request.open('POST', 'https://secret-about-box.herokuapp.com/sendLocation', true);
 	request.onreadystatechange = function ()
 	{
-		if(request.state == 4 && request.status == 200)
+
+		console.log(request.readyState);
+		if(request.readyState == 4 && request.status == 200)
 		{
 			data = request.responseText;
 			message = JSON.parse(data);
+			//console.log(message[0]);
+			getLocation();
+
 		}
 
 	};
-	request.send(null);
+
+	var params = "login=RobertHeller&lat=" + mycurrentLat + "&lng="+ mycurrentLng + "&message=" +"Hello%20world";
+	// set headers
+	//params = "<Your location in the right format>"
+	request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	request.send(params);
 }
